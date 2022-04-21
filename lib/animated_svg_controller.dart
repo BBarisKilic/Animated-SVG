06133cf1d0baa3cd65f3a4a565ffaec8e1b8f6f4
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-04-22 02:16:41 +0300

    Fixed late initialization issue.

--- lib/animated_svg_controller.dart
+++ lib/animated_svg_controller.dart
@@ -3,9 +3,9 @@
 // can be found in the LICENSE file.
 
 class AnimatedSvgController {
-  late final bool Function() reverse;
-  late final bool Function() forward;
-  late final bool Function() isDismissed;
-  late final bool Function() isAnimating;
-  late final bool Function() isCompleted;
+  late bool Function() reverse;
+  late bool Function() forward;
+  late bool Function() isDismissed;
+  late bool Function() isAnimating;
+  late bool Function() isCompleted;
 }
